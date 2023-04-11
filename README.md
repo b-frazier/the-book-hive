c3e738a97eb9fb475193487ac992e150cbddaa36
bfrazier <bthehippie2@gmail.com>
2023-04-10 20:49:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Book Hive
+
+## Description
+
+A single page MERN stack application to search and save books to a personal catalog.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Installation
+
+tbd
+
+## Usage
+
+This should be used to manage a personal library of physical books or to create a digital wishlist.
+
+## License
+
+MIT ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)<br>
+https://opensource.org/licenses/MIT
+
+## Contributing
+
+Request access to the project. Create a new branch to open a pull request.
+
+## Questions
+
+Contact information for any questions:<br>
+bianca.frazier90@gmail.com<br>
+https://github.com/b-frazier/
+
+## Collaborators
+
+William Anderson<br>
+https://github.com/wllmAnderson/
+Hussein Jama<br>
+Serge Omadjambe<br>
+https://github.com/sergeomadjambe/
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { User } = require('../models');
+
+const resolvers = {
+  Query: {
+    me: async (p, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          '-__v -password'
+        );
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+  Mutation: {
+    addUser: async (p, { body }) => {
+      return await User.create(body);
+    },
+    login: async (p, { body }) => {
+      const user = await User.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
+
+      if (!user) {
+        throw new AuthenticationError('No user found');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (p, { newBook }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (p, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
