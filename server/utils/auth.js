0f2fc1a9b7d5a064adf2c2699d5288eb258feb4f
bfrazier <bthehippie2@gmail.com>
2023-04-13 20:24:30 -0400

    remove comments

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,17 +7,15 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+    // allows token to be sent via req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
       return req;
-      //return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -26,7 +24,6 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
