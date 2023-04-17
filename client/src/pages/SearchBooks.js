e81137f53a1917549bd5b7f4b1170760270462f9
bfrazier <bthehippie2@gmail.com>
2023-04-17 18:21:33 -0400

    remove console.logs

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -56,8 +56,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    console.log('searched', searchedBooks);
-    console.log('book:', bookToSave);
+
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
