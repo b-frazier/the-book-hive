46b5229f18f2dba45e3eb86712ea997106e6d865
bfrazier <bthehippie2@gmail.com>
2023-04-17 18:19:55 -0400

    uncomment code to run page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -42,14 +42,14 @@ const SavedBooks = () => {
       </div>
       <Container>
         {/* this code was breaking the build (probably because of auth issues) */}
-        {/* <h2 className="pt-5">
+        <h2 className="pt-5">
           {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${
                 userData.savedBooks.length === 1 ? 'book' : 'books'
               }:`
             : 'You have no saved books!'}
-        </h2> */}
-        {/* <Row>
+        </h2>
+        <Row>
           {userData.savedBooks.map((book) => {
             return (
               <Col md="4">
@@ -76,7 +76,7 @@ const SavedBooks = () => {
               </Col>
             );
           })}
-        </Row> */}
+        </Row>
       </Container>
     </>
   );
